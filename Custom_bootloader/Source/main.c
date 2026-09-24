0808ad6b2ccffba614947c582783e1f613588e3d
Likith R <likithrgowda8@gmail.com>
2026-09-24 22:51:05 +0530

    fixed folder structure

---
+++ Custom_bootloader/Source/main.c
@@ -0,0 +1,54 @@
+/**
+ * @file    main.c
+ * @brief   Brief description of the file.
+ *
+ * @details Detailed description of the file.
+ *
+ * @author  likith
+ * @date    24-Sept-2026
+ */
+
+
+/* ------------------------------------------------------------------Includes ------------------------------------------------------------------*/
+
+#include <stdint.h>
+/*------------------------------------------------------------------ Macros ------------------------------------------------------------------*/
+
+/* ------------------------------------------------------------------ Global variables ------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------ Local / static function prototypes ------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------ Global function  ------------------------------------------------------------------*/
+
+/**
+ * @brief  Brief description of the function.
+ *
+ * @param  parameter Description of parameter.
+ *
+ * @return Description of return value.
+ */
+
+int main()
+{
+	uint8_t a=10;
+	uint8_t b=20;
+
+	uint8_t c=0;
+
+	c=a+b;
+	while(1)
+	{
+		c++;
+	}
+}
+
+/*------------------------------------------------------------------ Local function  ------------------------------------------------------------------*/
+
+/**
+ * @brief  Brief description of the function.
+ *
+ * @param  parameter Description of parameter.
+ *
+ * @return Description of return value.
+ */
+
